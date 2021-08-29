f63db1b235e5bc44a7b6a4040cfbf30a515c6bfa
Jhonatan Rivas <jjrc.12@outlook.com>
2021-08-29 02:04:50 -0500

    e

---
+++ demomvc/Models/Ventas.cs
@@ -0,0 +1,14 @@
+namespace demomvc.Models
+{
+    public class Ventas
+    {
+       public string Name{ get; set; }
+
+       public string Categoria{ get; set; }
+
+       public string Precio{get; set;}
+
+       public string Descuento{get; set; }
+
+    }
+}
