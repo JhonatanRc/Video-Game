d7ea280f412195ccfb2cf670c9eff957454ac9fb
Jhonatan Rivas <jjrc.12@outlook.com>
2021-08-29 03:46:39 -0500

    formulario

---
+++ demomvc/Controllers/VentasController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using demomvc.Models;
+
+namespace demomvc.Controllers
+{
+    public class VentasController:Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            ViewData["Message"] = "Se registró con exito, gracias";
+            return View("index");
+        }
+
+    }
+}
